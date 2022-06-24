00423e65820c91f7d7cc510c69b896dae2c3ee7b
Vie <the_ruesi@zoho.com>
2022-06-24 22:01:22 +0100

    form added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
+import { useState } from 'react';
 import './App.css';
+import Header from './components/Header';
+import moment from 'moment';
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
+
+const App = () => {
+
+const [tasks, setTasks ] = useState([
+  {id: uuidv4(),  text: 'Walk the dog'},
+  {id: uuidv4(),  text: 'Kick the cat'}
+])
+
+  const [formValue, setFormValue] = useState('')
+
+  const onValueChange = (e) => {
+    e.preventDefault()
+    console.log(e.target.value)
+    setFormValue(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+
+    e.preventDefault();
+    console.log('Requesting a new task!')
+    console.log(formValue)
+    setTasks([...tasks, {id: uuidv4(), text: formValue}])
+    console.log(e);
+    setFormValue('')
+  }
+
+
+
+
   return (
     <div className="App">
-      <p>Hello there. </p>
+      <Header />
+      <div id="container">
+
+        {
+          tasks.map(task => <p key={task.id}>{task.text}</p>)
+        }
+        <div id="top-menu">
+          <form onSubmit={handleSubmit}>
+            <label>
+              Task Description:
+              <input type="text" value={formValue} onChange={onValueChange} />
+            </label>
+            <input type="submit" value="submit" />
+          </form>
+          
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Task Management</h1>
+      <nav>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
